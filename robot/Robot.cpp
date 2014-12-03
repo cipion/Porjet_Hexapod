efe9ea231e77ae6e71834d9edb4ac629c2506aec
cipion <thomas.gache@yahoo.fr>
2014-12-03 20:31:24 +0100

    MàJ Robot.js
    ajout des methodes

--- robot/Robot.cpp
+++ robot/Robot.cpp
@@ -1,11 +1,54 @@
 #include "Robot.h"
 
 
-Robot::Robot()
+Robot::Robot(GpioController gpioRPi)
 {
 }
 
 
 Robot::~Robot()
 {
+}
+
+
+void processDirectionRobot()
+{
+
+}
+
+
+void sendDirectionsPattes()
+{
+
+}
+
+
+static Robot getHandle()
+{
+
+}
+
+
+
+void originRobot()
+{
+
+}
+
+
+void MouvementX(int x_ws)
+{
+
+}
+
+
+void MouvementY(int y_ws)
+{
+
+}
+
+
+void MouvementZ(int z_ws)
+{
+
 }
--- robot/Robot.h
+++ robot/Robot.h
@@ -31,46 +31,205 @@
 class Robot
 {
 public:
-	Robot();
-	~Robot();
+    /**
+         * Constructeur de l'objet robot representant un ensemble de pattes
+         *
+         * @param gpioRPi
+         * 				Object permettant la manipulation du GPIO du Raspberry Pi
+         */
+    Robot(GpioController gpioRPi);
+    ~Robot();
+
+    /**
+         * Methode permettant de parametrer le timer par rapport au module des joysticks
+         */
+    void processDirectionRobot();
+
+    /**
+         * Methode cyclique permettant de mettre a jour les pattes (step + 1)
+         */
+    void sendDirectionsPattes();
+
+    /**
+         * Methode permettant d'obtenir une reference sur l'objet unique Robot
+         */
+    static Robot getHandle();
+
+
+    /**
+         * Methode permettant de mettre le robot en position de reference
+         */
+    void originRobot();
+
+    /**
+             * Methode permettant de mettre a jour la valeur du joystick gauche sur l'axe horizontal
+             *
+             * @param x_ws
+             * 			Valeur en int (-255 a 255) de la position du joystick gauche sur l'axe horizontal
+             */
+    void MouvementX(int x_ws);
+
+    /**
+                 * Methode permettant de mettre a jour la valeur du joystick gauche sur l'axe vertical
+                 *
+                 * @param y_ws
+                 * 			Valeur en int (-255 a 255) de la position du joystick gauche sur l'axe vertical
+                 */
+    void MouvementY(int y_ws);
+
+    /**
+                     * Methode permettant de mettre a jour la valeur du joystick droit sur l'axe horizontal
+                     *
+                     * @param z_ws
+                     * 			Valeur en int (-255 a 255) de la position du joystick droit sur l'axe horizontal
+                     */
+    void MouvementZ(int z_ws);
+
+
+
+
+
+
+
 
 private:
+
+    /**
+                         * Methode donnant le maximum des valeurs passe en parametres
+                         *
+                         * @param val1
+                         * 			Valeur 1 a tester
+                         *
+                         * @param val2
+                         * 			Valeur 2 a tester
+                         *
+                         * @param val3
+                         * 			Valeur 3 a tester
+                         *
+                         * @param val4
+                         * 			Valeur 4 a tester
+                         *
+                         * @param val5
+                         * 			Valeur 5 a tester
+                         *
+                         * @param val6
+                         * 			Valeur 6 a tester
+                         *
+                         * @return la valeur la plus eleve des valeurs passe en parametres
+                         */
+    static float getMax(float val1, float val2, float val3, float val4, float val5, float val6);
+
+    /**
+         * Methode retourant la longueur de la droite de mouvement pour le CIR
+         *
+         * @param dist
+         * 			Distance entre le CIR et la patte
+         *
+         * @param distMax
+         * 			Distance entre le CIR et la patte la plus eloigne
+         *
+         * @return la valeur de la droite de mouvement
+         */
+    static int getLongueurMovCIR(float dist, float distMax);
+
+
+    /**
+             * Methode retourant l'angle de la droite de mouvement pour le CIR
+             *
+             * @param distCIR
+             * 			Distance entre le CIR et l'origine du robot
+             *
+             * @param distHypPatte
+             * 			Distance entre le CIR et la patte
+             *
+             * @param distPatte
+             * 			Distance entre la patte et l'origine du robot
+             *
+             * @return la valeur de l'angle de la droite de mouvement
+             */
+    static int getAngleCIR(float distCIR, float distHypPatte, float distPatte);
+
+    /**
+                 * Methode permettant de retourner un module a partir de deux coordonnees cartesiennes
+                 *
+                 * @param x
+                 * 			Valeur en x de la coordonnee cartesienne
+                 *
+                 * @param y
+                 * 			Valeur en y de la coordonnee cartesienne
+                 *
+                 * @return module par rapport aux coordonnees
+                 */
+    static float module(float x, float y);
+
+    /**
+                     * Methode permettant de retourner un module a partir de deux coordonnees cartesiennes
+                     *
+                     * @param x
+                     * 			Valeur en x de la coordonnee cartesienne
+                     *
+                     * @param y
+                     * 			Valeur en y de la coordonnee cartesienne
+                     *
+                     * @return module par rapport aux coordonnees
+                     */
+    static int module(int x, int y);
+
+    /**
+                         * Methode permettant de retourner un angle a partir de deux coordonnees cartesiennes
+                         *
+                         * @param x
+                         * 			Valeur en x de la coordonnee cartesienne
+                         *
+                         * @param y
+                         * 			Valeur en y de la coordonnee cartesienne
+                         *
+                         * @return Angle en degre par rapport aux coordonnees
+                         */
+    static int arcTanDeg(int x, int y);
+
+
+
     int angleFL;
-       int angleFR;
-       int angleML;
-       int angleMR;
-       int angleBL;
-       int angleBR;
+    int angleFR;
+    int angleML;
+    int angleMR;
+    int angleBL;
+    int angleBR;
 
-       int longueurFL;
-       int longueurFR;
-       int longueurML;
-       int longueurMR;
-       int longueurBL;
-       int longueurBR;
+    int longueurFL;
+    int longueurFR;
+    int longueurML;
+    int longueurMR;
+    int longueurBL;
+    int longueurBR;
 
-       Timer timer;
-       long periodTimer;
+    Timer timer;
+    long periodTimer;
 
-       char vitesseServomoteurs;
+    char vitesseServomoteurs;
 
-       // Valeurs en INT de -255 a 255 des joysticks
-       int x_joy;
-       int y_joy;
-       int z_joy;
+    // Valeurs en INT de -255 a 255 des joysticks
+    int x_joy;
+    int y_joy;
+    int z_joy;
 
-       static Robot handle;
+    static Robot handle;
 
-       @SuppressWarnings("unused") // !!!
-       const GpioPinDigitalOutput modeBasc;
+    @SuppressWarnings("unused") // !!!
+    const GpioPinDigitalOutput modeBasc;
 
-       // Definition des pattes pour un Hexapod
-       Patte front_left;
-       Patte front_right;
-       Patte middle_left;
-       Patte middle_right;
-       Patte back_left;
-       Patte back_right;
+    // Definition des pattes pour un Hexapod
+    Patte front_left;
+    Patte front_right;
+    Patte middle_left;
+    Patte middle_right;
+    Patte back_left;
+    Patte back_right;
+
+
+
+
 
 };
 
